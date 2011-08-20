84aa6f2ea256d3bd787634643d7c61bd6f934157
Lawrence D'Oliveiro <ldo@geek-central.gen.nz>
2011-08-20 01:18:55 +0000

    add copyright notices

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,19 @@ package nz.gen.geek_central.Compass3D;
 /*
     Display a 3D compass arrow using OpenGL.
 
-    Written by Lawrence D'Oliveiro <ldo@geek-central.gen.nz>.
+    Copyright 2011 by Lawrence D'Oliveiro <ldo@geek-central.gen.nz>.
+
+    Licensed under the Apache License, Version 2.0 (the "License"); you may not
+    use this file except in compliance with the License. You may obtain a copy of
+    the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+    License for the specific language governing permissions and limitations under
+    the License.
 */
 
 public class Main extends android.app.Activity
